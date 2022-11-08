5c5a0966ee240bdced91b9ee1e8b7a0df64b92d2
simple github <foysalahmed5965@gmail.com>
2022-11-08 12:20:56 +0600

    loade single data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express= require('express');
 const cors= require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors())
@@ -23,6 +23,18 @@ async function run(){
             const cursor= serviceCollection.find(query);
             const result= await cursor.limit(3).toArray();
             res.send(result)
+        })
+        app.get('/allServices', async(req,res)=>{
+            const query= {};
+            const cursor= serviceCollection.find(query);
+            const result= await cursor.toArray();
+            res.send(result)
+        })
+        app.get('/allServices/:id', async(req,res)=>{
+            const id= req.params.id;
+            const query= {_id:ObjectId(id)}
+            const cursor=await serviceCollection.findOne(query);
+            res.send(cursor)
         })
 
     }

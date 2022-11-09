902801aa858c73375d770e031bfdc855a90f53ec
simple github <foysalahmed5965@gmail.com>
2022-11-09 21:13:36 +0600

    delete review

--- index.js
+++ index.js
@@ -67,6 +67,12 @@ async function run(){
             const result= await cursor.toArray();
             res.send(result)
         })
+        app.delete('/myreviews/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query= {_id:ObjectId(id)};
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result)
+        })
 
     }
     finally{
